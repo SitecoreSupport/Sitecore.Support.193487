86ce6260941793143a4f968d63fde8cad0cceec7
DK\ach <ach@sitecore.net>
2017-11-14 17:50:32 +0200

    patch commit

---
+++ src/Sitecore.Support.193487/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.193487")]
+[assembly: AssemblyProduct("Sitecore.Support.193487")]
+[assembly: ComVisible(false)]
